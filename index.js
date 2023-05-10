adaedcb91fa57c16592e7c32a5edf7f240c75fb4
IsaacDJohnson <isaac.designws@gmail.com>
2023-05-10 10:42:56 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin (element) {
+    for (const record of element) {
+      if (record.result === 'W') {
+        return (record.year);
+      }
+    }
+  }
